e199592f651dd6d348ead7a43159b5cfd66e2cdd
Luka Phirtskhalava <LukaPhirtskhalava5@gmail.com>
2021-02-02 00:13:48 +0400

    Get Random Background Color
    By clicking a button u can change body background-color, but changed color is random.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Simple JS Projects
  Simple projects or functionalities in JS 
+
+If u think that you can find big projects in this repository, u are quite wrong about that.
+I'm pushing simple projects and functionalities soo... Don't be mad if u dont find anything that entertains u.
+Have a nice day and keep coding <3
---
+++ RandomBgcChanger/main.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('btn')
+const backgroundClr = document.querySelector('body')
+
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+//This JS function takes random int or strings from hexNumber array and transforms it into hexCode.
+
+// I Can write a function like i do in this line but i can also write a function and call it after clicking on button.
+btn.addEventListener("click", function() {
+    let hexCol = '#'
+    // This loop takes random values from hexNumber array and sums it with with hexCol
+    for (var i = 0; i<6;i++) {
+        let random = Math.floor(Math.random()*hexNumbers.length)
+        hexCol += hexNumbers[random]        
+    }
+    backgroundClr.style.backgroundColor = hexCol 
+
+});
+
